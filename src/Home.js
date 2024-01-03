3661f2010fce69995da96183b7d82ab1641c7199
Reked37 <dtruesdale37@gmail.com>
2024-01-03 17:55:59 -0600

    added css to homepage

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,25 @@ export default function Home() {
   return (
     <div>
       <h1 className= 'title'> Derek Truesdale's Personal Website</h1>
-      <img className="picture" src='https://i.imgur.com/G9dZsHT.jpg' />
+      <div className='container'>
+        <img className="picture" src='https://i.imgur.com/G9dZsHT.jpg' />
+      </div>
       <h2>Who am I?</h2>
       <div>I am an indivdual who love to learn and wants to contribute to the world. I graduated from Kent State University with my bachelors in psychology
         While I was at school I particpated in many school activities such as co-found and playing for the Kent State Esports organization where I played league of legends for my college and
         recieved scholarship. After my first year of college I had the amazing opportunity to be a ⚡ Flashguide ⚡ or a student orientation leader where I helped welcome the new upcoming freshmen
         get acclimated to the university. Lastly, I had the opportunity to study abroad at the University of Liecester in England for a semester! Where I traveled to the city of Bath and Stonehedge as well as
         toured the Warner Brother Studio where Harry Potter was filmed. 
-      </div>
-      <img className="picture" src='https://i.imgur.com/jf9tGuC.jpg'/>
-      <img className="picture" src='https://i.imgur.com/wQEJiTf.jpg'/>
-      <img className="picture" src='https://i.imgur.com/4h5ziu2.jpg'/>
+      </div><br/>
+      <div className='container'>
+        <img className="school-pictures" src='https://i.imgur.com/jf9tGuC.jpg'/>
+        <img className="school-pictures" src='https://i.imgur.com/wQEJiTf.jpg'/><br/>
+      </div><br/>
+    <div className='container'>
+        <img className="esports" src='https://i.imgur.com/4h5ziu2.jpg'/>
+    </div>
 
-      <h2>Trajectory of Career</h2>
+      <h2 className='sub-title'>Trajectory of Career</h2>
       <div> I recenetly graduated from Flatiron School which is a coding bootcamp which taught me the fundamentals of what it means to be a software engineer
         I started my journey to be a software engineer in April of 2023 and completed my certifcation in November 2023. Now that I have completed my courses, I am particpating in Chingu, 
         which is a 6-week project that I get put into a team environment to grow my abilities further and see what it's like to work in a professional setting with a team. Currently I am looking
--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,11 @@ export default function Projects() {
   return (
     <div>
         <h1>Projects</h1>
+        <h3>Youtube Watch List</h3>
+        <div>Description of Watch List</div>
+        <h3>Password Manager</h3>
+        <div>De</div>
+
     </div>
   )
 }
